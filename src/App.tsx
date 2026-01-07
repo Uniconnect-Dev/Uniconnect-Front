ad122a3b1008a61af5d10891be239570a582158c
1eehvunzin <hyunjinlee@ewha.ac.kr>
2026-01-07 21:40:53 +0900

    feat: 학생단체-마이페이지-계약서작성 컴포넌트 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 // 로그인
 import Login from './auth/login/Login';
 
-SignUpComplete
+SignUpComplete;
 // 회원가입 공통
 import SelectUserType from './auth/signup/SelectUserType';
 import SignUpComplete from './auth/signup/SignUpComplete';
@@ -17,7 +17,6 @@ import Step3BusinessInfo from './auth/signup/corporate/Step3BusinessInfo';
 import Step1StaffInfo from './auth/signup/student/Step1StaffInfo';
 import Step2UnivInfo from './auth/signup/student/Step2UnivInfo';
 
-
 // 학생 쇼핑몰
 import Studentshopping from './app/student/Studentshopping';
 import CompanyDetail from './app/student/CompanyDetail';
@@ -27,11 +26,14 @@ import Order from './app/student/Order';
 import OrderComplete from './app/student/OrderComplete';
 
 // 학생 단체 제휴/샘플링 요청
-import Step1StudentInfo from './app/student/forms/Step1StudentInfo'; 
+import Step1StudentInfo from './app/student/forms/Step1StudentInfo';
 import Step2StudentEventInfo from './app/student/forms/Step2StudentEventInfo';
 import Step3ChooseCorporate from './app/student/forms/Step3ChooseCorporate';
 import Step4Agreements from './app/student/forms/Step4Agreements';
 import Step5Agreements from './app/student/forms/Step5Complete';
+
+//학생 마이페이지
+import StuContract from './app/student/mypage/Contract';
 
 // 기업 샘플링 요청
 import Step1BasicInfo from './app/corporate/samplingrequest/Step1BasicInfo';
@@ -39,9 +41,10 @@ import Step2TargetSet from './app/corporate/samplingrequest/Step2TargetSet';
 import Step3Matching from './app/corporate/samplingrequest/Step3Matching';
 import Step4Terms from './app/corporate/samplingrequest/Step4Terms';
 import StepDone from './app/corporate/samplingrequest/StepDone';
-import Contract from './app/corporate/mypage/Contract';
 
 //기업 마이페이지
+import CorpContract from './app/corporate/mypage/Contract';
+
 import Step5Complete from './app/student/forms/Step5Complete';
 
 export default function App() {
@@ -49,15 +52,12 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
-
       {/* 회원가입 */}
       <Route path="/signup" element={<SelectUserType />} />
       <Route path="/signup/complete" element={<SignUpComplete />} />
-
       {/* 학생 회원가입 */}
       <Route path="/signup/student/step1" element={<Step1StaffInfo />} />
       <Route path="/signup/student/step2" element={<Step2UnivInfo />} />
-
       {/* 기업 회원가입 */}
       <Route path="/signup/corporate/step1" element={<Step1CompanyInfo />} />
       <Route
@@ -65,7 +65,6 @@ export default function App() {
         element={<Step2BusinessLicense />}
       />
       <Route path="/signup/corporate/step3" element={<Step3BusinessInfo />} />
-
       {/* 학생 쇼핑몰 */}
       <Route path="/studentshopping" element={<Studentshopping />} />
       <Route path="/studentshopping/:companyId" element={<CompanyDetail />} />
@@ -75,17 +74,22 @@ export default function App() {
       />
       <Route path="/studentshopping/cart" element={<Cart />} />
       <Route path="/studentshopping/order" element={<Order />} />
-      <Route path="/studentshopping/order/complete" element={<OrderComplete />} />
-
+      <Route
+        path="/studentshopping/order/complete"
+        element={<OrderComplete />}
+      />
       {/* 학생 단체 제휴 / 샘플링 요청 */}
       <Route path="/studentsampling/step1" element={<Step1StudentInfo />} />
-      <Route path="/studentsampling/step2" element={<Step2StudentEventInfo />} />
+      <Route
+        path="/studentsampling/step2"
+        element={<Step2StudentEventInfo />}
+      />
       <Route path="/studentsampling/step3" element={<Step3ChooseCorporate />} />
       <Route path="/studentsampling/step4" element={<Step4Agreements />} />
       <Route path="/studentsampling/step5" element={<Step5Complete />} />
-
       Step5Complete
-
+      {/* 학생 단체 마이페이지*/}
+      <Route path="/studentmypage/contract" element={<StuContract />} />
       {/* 기업 샘플링 요청 */}
       <Route
         path="/corporatesamplingrequest/step1"
@@ -101,9 +105,8 @@ export default function App() {
       />
       <Route path="/corporatesamplingrequest/step4" element={<Step4Terms />} />
       <Route path="/corporatesamplingrequest/stepDone" element={<StepDone />} />
-
       {/* 기업 마이페이지*/}
-      <Route path="/corporatemypage/contract" element={<Contract />} />
+      <Route path="/corporatemypage/contract" element={<CorpContract />} />
     </Routes>
   );
 }
